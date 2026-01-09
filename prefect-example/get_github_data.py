28248bf33d8a5e9c07b6251b09aa17faa28f1efc
Miro5law <mir@xboxer.pl>
2026-01-09 11:15:45 +0100

    This is another testing part for dynamic parallel loading in prefect.

--- prefect-example/get_github_data.py
+++ prefect-example/get_github_data.py
@@ -1,15 +1,23 @@
 # Check if data was loaded from API into our own duck database
 import duckdb
 
-con = duckdb.connect(database= 'github_repos_issues.duckdb')
+con = duckdb.connect(database= 'github_dynamic_issues.duckdb')
 print( con.sql("describe"))
 
-cur = con.cursor()
+con3 = duckdb.connect(database= 'github_dynamic_releases.duckdb')
+print( con3.sql("describe"))
 
-cur.execute('SELECT  * FROM github_data.issues limit 1')
-results = cur.fetchall()
-print("Issue example", results)
+con2 = duckdb.connect(database= 'github_dynamic_repos.duckdb')
+print( con2.sql("describe"))
 
-cur.execute('SELECT  * FROM github_data.repos limit 1')
-results = cur.fetchall()
-print("Repo example", results)
+con4 = duckdb.connect(database= 'github_dynamic_contributors.duckdb')
+print( con4.sql("describe"))
+# cur = con.cursor()
+
+# cur.execute('SELECT  * FROM github_data.issues limit 1')
+# results = cur.fetchall()
+# print("Issue example", results)
+
+# cur.execute('SELECT  * FROM github_data.repos limit 1')
+# results = cur.fetchall()
+# print("Repo example", results)
